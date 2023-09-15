99810eed3c503bd4746017c43fb110dfa2035b35
shrutha28 <shruthas9@gmail.com>
2023-09-14 21:08:31 -0700

    First try

--- Snake_Game/Assets/FoodScript.cs
+++ Snake_Game/Assets/FoodScript.cs
@@ -13,9 +13,9 @@ public class FoodScript : MonoBehaviour
     public void RandomizePosition()
     {
         Bounds bounds = this.gridArea.bounds;
-        float x=Random.Range(bounds.min.x, bounds.max.x);
-        float y=Random.Range(bounds.min.y, bounds.max.y);
-        this.transform.position = new Vector3(Mathf.Round(x),Mathf.Round(y),0.0f);
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
